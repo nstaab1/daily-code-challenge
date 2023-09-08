82677aa0bde4f841c525a95849edacd0c1273743
Nathan Staab <nathanstaab@playbackrewards.com>
2023-09-08 13:39:38 -0400

    Upload code kata for 9/8/23

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-import { pigIt } from './daily-code-challenges/9-7-23';
+// import { pigIt } from './daily-code-challenges/9-7-23';
+import { reverseSeq } from './daily-code-challenges/9-8-23';
 
-const val = pigIt('Hello world !');
+// const val = pigIt('Hello world !');
+
+const val = reverseSeq(5);
+
+// eslint-disable-next-line no-console
+console.log(val);
