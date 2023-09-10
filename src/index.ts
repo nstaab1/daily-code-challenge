b950fa42f5507d6dc725d09644b590d430d3db7b
Nathan Staab <nathanstaab@playbackrewards.com>
2023-09-10 17:41:55 -0400

    Code challenge for Sept 10 2023

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 // import { pigIt } from './daily-code-challenges/9-7-23';
 import { reverseSeq } from './daily-code-challenges/9-8-23';
+import { pipeFix } from './daily-code-challenges/9-9-23';
 
 // const val = pigIt('Hello world !');
 
-const val = reverseSeq(5);
+// const val = reverseSeq(5);
+const val = pipeFix([-1, 4]);
 
 // eslint-disable-next-line no-console
 console.log(val);
