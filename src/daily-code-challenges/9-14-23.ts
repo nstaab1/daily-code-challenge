a56cf213d267fac2be8742b75a54a19753d5ab66
Nathan Staab <nathanstaab@playbackrewards.com>
2023-09-14 14:16:08 -0400

    Code challenge for Spet 14 23

---
+++ src/daily-code-challenges/9-14-23.ts
@@ -0,0 +1,78 @@
+// Create a Vector object that supports addition, subtraction, dot products, and norms. So, for example:
+
+// a = new Vector([1, 2, 3])
+// b = new Vector([3, 4, 5])
+// c = new Vector([5, 6, 7, 8])
+
+// a.add(b)      # should return a new Vector([4, 6, 8])
+// a.subtract(b) # should return a new Vector([-2, -2, -2])
+// a.dot(b)      # should return 1*3 + 2*4 + 3*5 = 26
+// a.norm()      # should return sqrt(1^2 + 2^2 + 3^2) = sqrt(14)
+// a.add(c)      # throws an error
+// If you try to add, subtract, or dot two vectors with different lengths, you must throw an error!
+
+// Also provide:
+
+// a toString method, so that using the vectors from above, a.toString() === '(1,2,3)' (in Python, this is a __str__ method, so that str(a) == '(1,2,3)')
+// an equals method, to check that two vectors that have the same components are equal
+// Note: the test cases will utilize the user-provided equals method.
+
+export class Vector {
+  vectorParams: number[];
+  public constructor(components: number[]) {
+    this.vectorParams = components;
+  }
+
+  public add = (vector: Vector) => {
+    this.checkEqualLength(vector);
+    const newParams = this.vectorParams.map((num, i) => {
+      return num + vector.vectorParams[i];
+    });
+    return new Vector(newParams);
+  };
+
+  public subtract = (vector: Vector) => {
+    this.checkEqualLength(vector);
+    const newParams = this.vectorParams.map((num, i) => {
+      return num - vector.vectorParams[i];
+    });
+    return new Vector(newParams);
+  };
+
+  public dot = (vector: Vector) => {
+    this.checkEqualLength(vector);
+    let sum = 0;
+    this.vectorParams.map((num, i) => {
+      sum += num * vector.vectorParams[i];
+    });
+    return sum;
+  };
+
+  public norm = () => {
+    let sum = 0;
+    this.vectorParams.map((num) => {
+      sum += Math.pow(num, 2);
+    });
+    return Math.sqrt(sum);
+  };
+
+  public equals = (vector: Vector) => {
+    let isEqual = true;
+    this.vectorParams.map((num, i) => {
+      if (vector.vectorParams[i] && num != vector.vectorParams[i]) {
+        isEqual = false;
+      }
+    });
+    return isEqual;
+  };
+
+  public toString = () => {
+    return `(${this.vectorParams.join()})`;
+  };
+
+  private checkEqualLength = (vector: Vector) => {
+    if (vector.vectorParams.length !== this.vectorParams.length) {
+      throw Error('Vectors not same length');
+    }
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,14 @@
 // import { reverseSeq } from './daily-code-challenges/9-8-23';
 // import { pipeFix } from './daily-code-challenges/9-9-23';
 // import { evenOrOdd } from './daily-code-challenges/9-11-23';
-import { makeLooper } from './daily-code-challenges/9-12-23';
+// import { makeLooper } from './daily-code-challenges/9-12-23';
+import { Vector } from './daily-code-challenges/9-14-23';
 
-// const val = pigIt('Hello world !');
-
-// const val = reverseSeq(5);
-// const val = pipeFix([-1, 4]);
-const looper = makeLooper('asdas');
-
-const looper2 = makeLooper('as');
-const val = looper();
-
-const val2 = looper();
-
-const val4 = looper2();
-
-const val3 = looper();
-
-const val5 = looper2();
-const val6 = looper2();
-
+const a = new Vector([1, 2, 3]);
+const b = new Vector([3, 4, 5]);
+const c = new Vector([5, 6, 7, 8]);
+const d = new Vector([1, 2, 3]);
 // eslint-disable-next-line no-console
-console.log(val, val2, val3);
+console.log(a.norm());
 
-console.log(val4, val5, val6);
+console.log(a.toString());
